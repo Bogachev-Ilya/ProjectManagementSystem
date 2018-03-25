e7a768c273f3def99ae989bc3086d4659bcc95ba
Ilya Bogachev <bogachev_i_i@mail.ru>
2018-03-25 17:14:46 +0300

    add @Before and @After methods for initialization of tables

--- src/Tables.java
+++ src/Tables.java
@@ -8,37 +8,30 @@ public class Tables {
         return resultSet;
     }
 
+    public void setResultSet(ResultSet resultSet) {
+        this.resultSet = resultSet;
+        }
+
     public void createTablesAndInit(String URL, String tableName) {
-
         try (Connection connection = DriverManager.getConnection(URL)) {
-            PreparedStatement statement = connection.prepareStatement("DROP TABLE IF EXISTS " + tableName + ";");
-
-            statement.executeUpdate();
-            PreparedStatement statement1 = connection.prepareStatement("CREATE TABLE IF NOT EXISTS " + tableName +
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DROP TABLE IF EXISTS " + tableName + ";");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS " + tableName +
                     "(Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Project TEXT, Task TEXT, Responsible TEXT, Phone_Email TEXT, StartDate TEXT, Task_Duration INTEGER , IS_TaskComplite INTEGER);");
-            statement1.executeUpdate();
-            PreparedStatement statement2 = connection.prepareStatement("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
+            statement.executeUpdate("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
                     "VALUES ('NotStandardJava', 'Create new Lib', 'Bob', '456-789-00, bob@java.net', '2017-01-01', 720, 0);");
-            PreparedStatement statement3 = connection.prepareStatement("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
+            statement.executeUpdate("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
                     "VALUES ('NotStandardJava', 'Update Standard Classes', 'Paul', '456-789-01, paul@java.net', '2017-06-20', 180, 1);");
-            PreparedStatement statement4 = connection.prepareStatement("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
+            statement.executeUpdate("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
                     "VALUES ('Browser FirsFast', 'GUI', 'Jak', '456-789-12, jak@java.net', '2016-03-17', 720, 1);");
-            PreparedStatement statement5 = connection.prepareStatement("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
+            statement.executeUpdate("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
                     "VALUES ('Android App', 'Network', 'Piter', '456-789-99, piter@java.net', '2018-03-19', 150, 0);");
-            PreparedStatement statement6 = connection.prepareStatement("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
+            statement.executeUpdate("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
                     "VALUES ('Android App', 'Visual Style', 'Julia', '456-789-74, julia@java.net', '2017-11-03', 210, 0);");
-            PreparedStatement statement7 = connection.prepareStatement("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
+            statement.executeUpdate("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
                     "VALUES ('Android App', 'App logic', 'Troy', '456-000-01, troy@java.net', '2017-02-10', 365, 0);");
-            PreparedStatement statement8 = connection.prepareStatement("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
+            statement.executeUpdate("INSERT INTO " + tableName + " (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
                     "VALUES ('Android App', 'Transaction', 'Jak', '456-789-12, jak@java.net', '2017-05-20', 150, 1);");
-
-            statement2.executeUpdate();
-            statement3.executeUpdate();
-            statement4.executeUpdate();
-            statement5.executeUpdate();
-            statement6.executeUpdate();
-            statement7.executeUpdate();
-            statement8.executeUpdate();
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -54,10 +47,10 @@ public class Tables {
                 String name = resultSet.getString("Project");
                 System.out.printf("Projects in work: %s\n", name);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 
     public void countIncompleteProjectTasks(String URL, String tableName, String projectName) {
--- test/TablesTest.java
+++ test/TablesTest.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,49 +8,65 @@ import static org.junit.Assert.*;
 
 public class TablesTest {
     Tables tables;
-
+    Connection connection;
+    Statement statement;
     @Before
     public void setUp() throws Exception {
-
-
         try {
             Class.forName("org.sqlite.JDBC");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        try  {
+            connection = DriverManager.getConnection("jdbc:sqlite:test.db");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        statement = connection.createStatement();
         tables = new Tables();
         tables.createTablesAndInit("jdbc:sqlite:test.db", "ProjectManagment");
+        statement.executeUpdate("DROP TABLE IF EXISTS PM");
+        statement.executeUpdate("CREATE TABLE IF NOT EXISTS PM" +
+                "(Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Project TEXT, Task TEXT, Responsible TEXT, Phone_Email TEXT, StartDate TEXT, Task_Duration INTEGER, IS_TaskComplite INTEGER);");
+
+        statement.executeUpdate("INSERT INTO PM (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
+                "VALUES ('NotStandardJava', 'Create new Lib', 'Bob', '456-789-00, bob@java.net', '2017-01-01', 720, 0);");
+        statement.executeUpdate("DROP TABLE IF EXISTS IncompleteTasks");
+        statement.executeUpdate("CREATE TABLE IF NOT EXISTS IncompleteTasks" +
+                "(Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Project TEXT, Task TEXT, Responsible TEXT, Phone_Email TEXT, StartDate TEXT, Task_Duration INTEGER, IS_TaskComplite INTEGER);");
+        statement.executeUpdate("INSERT INTO IncompleteTasks (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
+                "VALUES ('NotStandardJava', 'Create new Lib', 'Bob', '456-789-00, bob@java.net', '2018-03-20', 180, 0);");
+
+
+    }
+    @After
+    public void closeTable(){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        tables.setResultSet(null);
     }
 
 
     @Test
     public void createTablesAndInit() throws SQLException {
-
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:test.db")) {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT Project FROM ProjectManagment WHERE Task = 'GUI';");
             while (resultSet.next()) {
                 String name = resultSet.getString("Project");
                 assertEquals("Browser FirsFast", name);
             }
-        }
     }
 
     @Test
     public void testShowProjectInWork() {
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate("DROP TABLE IF EXISTS PM");
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS PM" +
-                    "(Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Project TEXT, Task TEXT, Responsible TEXT, Phone_Email TEXT, StartDate TEXT, Task_Duration INTEGER, IS_TaskComplite INTEGER);");
-
-            statement.executeUpdate("INSERT INTO PM (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
-                    "VALUES ('NotStandardJava', 'Create new Lib', 'Bob', '456-789-00, bob@java.net', '2017-01-01', 720, 0);");
+        try  {
             ResultSet resultSet = statement.executeQuery("SELECT DISTINCT Project FROM PM WHERE IS_TaskComplite = 0;");
             String name = resultSet.getString("Project");
             tables.showProjectInWork("jdbc:sqlite:test.db", "PM");
             assertEquals(name, tables.getResultSet().getString("Project"));
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -57,14 +74,7 @@ public class TablesTest {
 
     @Test
     public void testCountIncompleteProjectTasks() {
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate("DROP TABLE IF EXISTS IncompleteTasks");
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS IncompleteTasks" +
-                    "(Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Project TEXT, Task TEXT, Responsible TEXT, Phone_Email TEXT, StartDate TEXT, Task_Duration INTEGER, IS_TaskComplite INTEGER);");
-
-            statement.executeUpdate("INSERT INTO IncompleteTasks (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
-                    "VALUES ('NotStandardJava', 'Create new Lib', 'Bob', '456-789-00, bob@java.net', '2017-01-01', 365, 0);");
+        try {
             ResultSet resultSet = statement.executeQuery("SELECT COUNT (Task) FROM IncompleteTasks WHERE (IS_TaskComplite =0 AND Project='NotStandardJava');");
             int count = resultSet.getInt(1);
             tables.countIncompleteProjectTasks("jdbc:sqlite:test.db", "IncompleteTasks", "NotStandardJava");
@@ -76,8 +86,7 @@ public class TablesTest {
 
     @Test
     public void testIncompleteResponsibleTasks() {
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement statement = connection.createStatement();
+        try  {
             ResultSet resultSet = statement.executeQuery("SELECT Task FROM IncompleteTasks WHERE (Responsible='Bob' AND IS_TaskComplite =0);");
             String tasks = resultSet.getString("Task");
             tables.incompleteResponsibleTasks("jdbc:sqlite:test.db", "IncompleteTasks", "Bob");
@@ -89,22 +98,11 @@ public class TablesTest {
 
     @Test
     public void testCurrentTasks() {
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate("DROP TABLE IF EXISTS IncompleteTasks");
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS IncompleteTasks" +
-                    "(Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Project TEXT, Task TEXT, Responsible TEXT, Phone_Email TEXT, StartDate TEXT, Task_Duration INTEGER, IS_TaskComplite INTEGER);");
-
-            statement.executeUpdate("INSERT INTO IncompleteTasks (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
-                    "VALUES ('NotStandardJava', 'Create new Lib', 'Bob', '456-789-00, bob@java.net', '2017-01-01', 365, 0);");
-            statement.executeUpdate("INSERT INTO IncompleteTasks (Project, Task, Responsible, Phone_Email, StartDate, Task_Duration, IS_TaskComplite)" +
-                    "VALUES ('NotStandardJava', 'Create API', 'Ron', '456-789-22, ron@java.net', '2018-03-20', 180, 0);");
+        try  {
             ResultSet resultSet = statement.executeQuery("SELECT DISTINCT Task, Responsible FROM IncompleteTasks WHERE (StartDate<DATE ('now') AND IS_TaskComplite =0);");
             String tasks = resultSet.getString("Task");
             tables.currentTasks("jdbc:sqlite:test.db", "IncompleteTasks");
-
             assertEquals(tasks, tables.getResultSet().getString("Task"));
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -112,14 +110,11 @@ public class TablesTest {
 
     @Test
     public void testLateTasks(){
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            Statement statement = connection.createStatement();
-
+        try  {
             ResultSet resultSet = statement.executeQuery("SELECT Responsible, Phone_Email FROM IncompleteTasks WHERE ((StartDate+Task_Duration)<DATE ('now') AND IS_TaskComplite =0);");
             String name = resultSet.getString("Responsible");
             tables.lateTasks("jdbc:sqlite:test.db", "IncompleteTasks");
             assertEquals(name, tables.getResultSet().getString("Responsible"));
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
